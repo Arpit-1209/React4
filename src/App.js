c745ec9f774c0e49ab31d47f818e58dd9de35275
Arpit Srivastava <112778916+Arpit-1209@users.noreply.github.com>
2023-10-15 12:53:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Table from './components/Table';
-import "C:/Users/Arpit Srivastava/OneDrive/Desktop/MERN fullstack/node_modules/bootstrap/dist/css/bootstrap.min.css";
+//import "C:/Users/Arpit Srivastava/OneDrive/Desktop/MERN fullstack/node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Table/>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" ></link>    
     </div>
   );
 }
